35faade882dbfcbbfe0b1a9f5a610bedb767dca1
29126229 <vickychepkorirchelangat1994@gmail.com>
2024-01-21 05:30:42 -0800

    5. Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - func checks for a digit (0 through 9)
+ * print_most_numbers - func checks for checks for a digit (0 through 9)
  *
  * Return: Always 0.
  */
