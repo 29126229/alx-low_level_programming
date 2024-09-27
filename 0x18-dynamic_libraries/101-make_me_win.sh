94bcc37d521d0525fe57af6fdaaa217d9247b1db
root <vickychepkorirchelangat1994@gmail.com>
2024-09-27 02:07:27 -0700

    dynamic liblaries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -fPIC -o win.so win.c
-LD_PRELOAD=$PWD/win.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
